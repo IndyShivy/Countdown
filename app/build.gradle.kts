418ef23e44ef8ecb107a730bc4656c5b14500d04
IndyShivy <chibixryuu@gmail.com>
2024-02-08 16:00:41 +1100

    - Update version for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.indyinc.countdown"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
